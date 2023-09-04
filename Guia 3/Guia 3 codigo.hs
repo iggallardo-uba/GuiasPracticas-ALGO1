a29bf8b8a7713eeec64caf4edbe7f180b29d67e9
test_user <test@example.com>
2023-09-04 11:23:25 -0300

    Guia 3 Terminada

--- Guia 3/Guia 3 codigo.hs
+++ Guia 3/Guia 3 codigo.hs
@@ -93,7 +93,96 @@ estanRelacionados x y | mod x y == 0 = True
 
 --EJERCICIO 4
 
---siguiente clase ya que ni idea de duplas
+--A
+prodInt :: (Integer, Integer) -> Integer
+prodInt (x,y) = x * y
 
---prodInt (Integer, Integer) -> (Integer, Integer) -> Float
---prodInt x y = x[0]
+--B
+todoMenor :: (Integer, Integer) -> (Integer, Integer) -> Bool
+todoMenor (x,y) (i,j) | x < i && y < j = True
+                      | otherwise = False
+
+--C
+distanciaPuntos :: (Float, Float) -> (Float, Float) -> Float
+distanciaPuntos (x,y) (i,j) = sqrt ((i-x)*(i-x)+(j-y)*(j-y))
+
+--D
+sumaTerna :: (Integer, Integer, Integer) -> Integer
+sumaTerna (x,y,z) = x+y+z
+
+ --E
+sumarSoloMultiplos :: (Integer, Integer, Integer) -> Integer -> Integer
+sumarSoloMultiplos (x,y,z) i | esMultiploDe x i && esMultiploDe y i && esMultiploDe z i = sumaTerna(x,y,z)
+                             | esMultiploDe x i && esMultiploDe y i = x + y
+                             | esMultiploDe y i && esMultiploDe z i = y + z
+                             | esMultiploDe x i && esMultiploDe z i = z + y
+                             | esMultiploDe x i = x
+                             | esMultiploDe y i = y
+                             | esMultiploDe z i = z
+                             | otherwise = 0
+
+--F
+posPrimerPar :: (Integer, Integer, Integer) -> Integer
+posPrimerPar (x,y,z) | esPar x = 1
+                     | esPar y = 2
+                     | esPar z = 3
+                     | otherwise = 4
+
+--FA (Funcion Auxiliar)
+esPar :: Integer -> Bool
+esPar x | esMultiploDe x 2 = True
+        | x == 0 = True
+        | otherwise = False
+
+--G y --H
+--Como que elementos de cualquier Tipo?
+--Ignoras la sintaxis de Tipos y dejas que Haskell lo opere
+
+--G
+crearPar :: a -> b -> (a,b)
+crearPar x y =  (x,y)
+
+--H
+invertir :: (a,b) -> (b,a)
+invertir (x,y) = (y,x)
+
+--Ejercicio 5
+todosMenores :: (Integer, Integer, Integer) -> Bool
+todosMenores (x, y, z) | esMayor (f1 x) (g1 x) && esMayor (f1 y) (g1 y) && esMayor (f1 z) (g1 z) = True
+                     | otherwise = False
+
+f1 :: Integer -> Integer
+f1 x | x <= 7 = x * x
+     | otherwise= 2 * x - 1
+
+g1 :: Integer -> Integer
+g1 x | esPar x = div x 2
+     | otherwise = 3 * x + 1
+
+--FA
+esMayor :: Integer -> Integer -> Bool
+esMayor x y | x > y = True
+            | otherwise = False
+
+--Ejercicio 6
+bisiesto :: Integer -> Bool
+bisiesto x | not(esMultiploDe x 4) || esMultiploDe x 100 && not(esMultiploDe x 400) = False   
+           | otherwise = True
+
+--Ejercicio 7
+distanciaManhattan:: (Float, Float, Float) ->(Float, Float, Float) ->Float
+distanciaManhattan (x,y,z) (i,j,k) = absolutoFloat (x-i) + absolutoFloat (y-j) + absolutoFloat (z-k)
+
+--FA
+absolutoFloat :: Float -> Float
+absolutoFloat x | x < 0 = x*(-1)
+                | otherwise = x
+
+--Ejercicio 8
+comparar :: Integer -> Integer -> Integer
+comparar x y | sumaUltimosDosDigitos x < sumaUltimosDosDigitos y = 1
+             | sumaUltimosDosDigitos x > sumaUltimosDosDigitos y = -1
+             | sumaUltimosDosDigitos x == sumaUltimosDosDigitos y = 0
+
+sumaUltimosDosDigitos :: Integer -> Integer
+sumaUltimosDosDigitos x = mod x 10 + mod (div x 10) 10
