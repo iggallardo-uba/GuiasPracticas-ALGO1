d297a6510b6ac1dc3c406617e33edd30d4e1491c
maite <maitena591@gmail.com>
2023-09-14 10:41:51 -0300

    CAsi se termina Guia 5

--- Guia 5/Guia 5 codigo.hs
+++ Guia 5/Guia 5 codigo.hs
@@ -3,7 +3,6 @@ esPrimo :: Integer -> Bool
 esPrimo x | busquedaDivisorEsPrimo x 2 == True = True
           | otherwise = False
 
---FA
 busquedaDivisorEsPrimo :: Integer -> Integer -> Bool
 busquedaDivisorEsPrimo x y | x == y = True
                            | mod x y == 0 = False
@@ -24,7 +23,7 @@ calculoSigPrimo x | esPrimo (x + 1) == True = x + 1
 --1
 longitud :: [t] -> Integer
 longitud [] = 0
-longitud (x : xs) = 1 + longitud xs
+longitud x = 1 + longitud (tail x)
 
 --2
 ultimo :: [t] -> t
@@ -33,7 +32,9 @@ ultimo (x : xs) | longitud xs == 0 = x
 
 --3
 principio :: (Num t) => [t] -> [t]
-principio (x : xs) = [x, 0, ultimo xs]
+principio x | longitud x == 0 = [0,0,0]
+            | longitud x == 1 = [head x, 0, head x]
+            | otherwise = [head x, 0, ultimo x]
 
 --4
 reverso :: (Eq t) => [t] -> [t]
@@ -52,45 +53,47 @@ pertenece x (y : ys) | y == x = True
 
 --2
 todosIguales :: (Eq t) => [t] -> Bool
-todosIguales (x : xs) = calculoTodosIguales xs x
-
-calculoTodosIguales :: (Eq t) => [t] -> t -> Bool
-calculoTodosIguales [] _ = True
-calculoTodosIguales (x : xs) y | x /= y = False
-                               | otherwise = calculoTodosIguales xs y
+todosIguales [] = True
+todosIguales (x : xs) | longitud xs == 0 = True
+                      | x /= head xs = False
+                      | otherwise = todosIguales xs
 
 --3
 todosDistintos :: (Eq t) => [t] -> Bool
-todosDistintos (x : xs) =  calculoTodosDistintos xs x
-
-calculoTodosDistintos :: (Eq t) => [t] -> t -> Bool
-calculoTodosDistintos [] _ = True
-calculoTodosDistintos x y | pertenece y x == True = False
-                          | otherwise = calculoTodosDistintos (tail x) (head x)
+todosDistintos [] = True
+todosDistintos (x : xs) | longitud xs == 0 = True
+                        | pertenece x xs == True = False
+                        | otherwise = todosDistintos xs
                                             
 --4
 hayRepetidos :: (Eq t) => [t] -> Bool
-hayRepetidos x = calculoHayRepetidos x
+hayRepetidos [] = False
+hayRepetidos (x : xs) | pertenece x xs == True = True
+                      | otherwise = hayRepetidos xs
 
-calculoHayRepetidos :: (Eq t) => [t] -> Bool
-calculoHayRepetidos [] = False
-calculoHayRepetidos (x : xs) | pertenece x xs == True = True
-                             | otherwise = calculoHayRepetidos xs
 
 --5
 quitar :: (Eq t) => t -> [t] -> [t]
-quitar x y = calcularQuitar x y [] 0
+quitar _ [] = []
+quitar x (y : ys) | x == y = ys
+                  | otherwise = y : (quitar x ys)
 
-calcularQuitar :: (Eq t) => t -> [t] -> [t] -> Integer -> [t]
-calcularQuitar _ [] z _ = z
-calcularQuitar _ y z 1 = y ++ z
-calcularQuitar x (y : ys) z c | x == y = calcularQuitar x ys z 1
-                              | otherwise = calcularQuitar x ys (y : z) 0
+--6
+quitarTodos :: (Eq t) => t -> [t] -> [t]
+quitarTodos _ [] = []
+quitarTodos x (y : ys) | x == y = quitarTodos x ys
+                       | otherwise = y : (quitarTodos x ys)
 
 --Ejercicio 3
 --1
+sumatoria :: [Integer] -> Integer
+sumatoria [] = 0
+sumatoria (x : xs) = x + sumatoria xs
 
 --2
+productoria :: [Integer] -> Integer
+productoria [] = 1
+productoria (x : xs) = x * productoria xs
 
 --3
 maximo :: [Integer] -> Integer
@@ -100,6 +103,33 @@ calcularMaximo :: [Integer] -> Integer -> Integer
 calcularMaximo [] x = x
 calcularMaximo (x : xs) y | x > y = calcularMaximo xs x
                           | otherwise = calcularMaximo xs y
+
+--4
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN _ [] = []
+sumarN x (y : ys) = (y + x) : (sumarN x ys)
+
+--5
+sumarElPrimero :: [Integer] -> [Integer]
+sumarElPrimero [] = []
+sumarElPrimero x = sumarN (head x) x
+
+--6
+sumarElUltimo :: [Integer] -> [Integer]
+sumarElUltimo [] = []
+sumarElUltimo x = sumarN (ultimo x) x
+
+--7
+pares :: [Integer] -> [Integer]
+pares [] = []
+pares (x : xs) | mod x 2 == 0 = x : pares xs
+               | otherwise = pares xs
+
+--8
+multiplosDeN :: Integer -> [Integer] -> [Integer]
+multiplosDeN _ [] = []
+multiplosDeN x (y : ys) | mod y x == 0 = y : multiplosDeN x ys
+                        | otherwise = multiplosDeN x ys
 
 --9
 ordenar :: [Integer] -> [Integer]
@@ -111,6 +141,13 @@ calcularOrdenar x y = calcularOrdenar (quitar (maximo x) x) (maximo x : y)
 
 --Ejercicio 5
 --1
+sumaAcumulada :: (Num t) => [t] -> [t]
+sumaAcumulada [] = []
+sumaAcumulada x = calcularSumaAcumulada 0 x
+
+calcularSumaAcumulada :: (Num t) => t -> [t] -> [t]
+calcularSumaAcumulada _ [] = []
+calcularSumaAcumulada x (y : ys) = calcularSumaAcumulada x + y
 
 --2
 descomponerEnPrimos :: [Integer] -> [[Integer]]
